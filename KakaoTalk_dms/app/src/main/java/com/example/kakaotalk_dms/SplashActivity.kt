72da18995d55a990f79ffb0e990fcf5f66d90651
seokjun <tjrwns8024@naver.com>
2019-11-18 18:49:03 +0900

    SplashImage

---
+++ KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.kakaotalk_dms
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import java.lang.Exception
+
+class SplashActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        try {
+            Thread.sleep(3000)
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        catch (e:Exception){
+            return
+        }
+    }
+}

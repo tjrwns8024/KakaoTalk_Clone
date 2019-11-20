0d39fdc883ae9fed06a4577f079a99d9e9debe42
seokjun <tjrwns8024@naver.com>
2019-11-20 19:48:26 +0900

    signin_layout

---
+++ KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/Signin.kt
@@ -0,0 +1,12 @@
+package com.example.kakaotalk_dms
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Signin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}

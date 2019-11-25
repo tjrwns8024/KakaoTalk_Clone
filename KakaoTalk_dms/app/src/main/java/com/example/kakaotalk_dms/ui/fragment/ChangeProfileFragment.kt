2679fa667afd877d06cfec50619b8cf86039d88b
seokjun <tjrwns8024@naver.com>
2019-11-25 19:06:56 +0900

    anim 적용

--- KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/fragment/ChangeProfileFragment.kt
+++ KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/fragment/ChangeProfileFragment.kt
@@ -1,36 +1,29 @@
 package com.example.kakaotalk_dms.ui.fragment
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentManager
 
 import com.example.kakaotalk_dms.R
 import kotlinx.android.synthetic.main.fragment_change_profile.*
-import kotlinx.android.synthetic.main.fragment_setting.*
 
-class SettingFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+class ChangeProfileFragment : Fragment() {
 
-        }
+    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_change_profile, container, false)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        change_profile.setOnClickListener{
+        change_profile_back_btn.setOnClickListener {
             val transaction = activity?.supportFragmentManager?.beginTransaction()?.setCustomAnimations(R.anim.fade_in,R.anim.fade_out)
-            transaction?.replace(R.id.main_frame,ChangeProfileFragment())
-            transaction?.addToBackStack(null)
+            transaction?.replace(R.id.main_frame,SettingFragment())
             transaction?.commit()
-
         }
     }
 }
-
-
 
 

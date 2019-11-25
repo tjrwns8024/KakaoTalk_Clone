d21dca8cc8a00f284997a6f7bda594afef68063b
seokjun <tjrwns8024@naver.com>
2019-11-25 20:25:04 +0900

    Setting intent

--- KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/fragment/SettingFragment.kt
+++ KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/fragment/SettingFragment.kt
@@ -1,19 +1,24 @@
 package com.example.kakaotalk_dms.ui.fragment
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.FragmentManager
 
 import com.example.kakaotalk_dms.R
+import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.fragment_change_profile.*
 import kotlinx.android.synthetic.main.fragment_setting.*
 
 class SettingFragment : Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
         return inflater.inflate(R.layout.fragment_setting, container, false)
 
         }
@@ -27,6 +32,11 @@ class SettingFragment : Fragment() {
             transaction?.addToBackStack(null)
             transaction?.commit()
 
+        }
+        check_id.setOnClickListener{
+            val transaction = activity?.supportFragmentManager?.beginTransaction()?.setCustomAnimations(R.anim.fade_in,R.anim.fade_out)
+            transaction?.replace(R.id.main_frame,ChangeIdFragment())
+            transaction?.commit()
         }
     }
 }

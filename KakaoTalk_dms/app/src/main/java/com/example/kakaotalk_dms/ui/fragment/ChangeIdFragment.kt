0cc67b66abc5f489d5801ccbbfcd801093da6dd7
seokjun <tjrwns8024@naver.com>
2019-11-26 22:34:19 +0900

    alertDialog

--- KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/fragment/ChangeIdFragment.kt
+++ KakaoTalk_dms/app/src/main/java/com/example/kakaotalk_dms/ui/fragment/ChangeIdFragment.kt
@@ -1,5 +1,6 @@
 package com.example.kakaotalk_dms.ui.fragment
 
+import android.app.AlertDialog
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 
 import com.example.kakaotalk_dms.R
 import kotlinx.android.synthetic.main.fragment_change_id.*
@@ -26,5 +28,20 @@ class ChangeIdFragment : Fragment() {
                transaction?.replace(R.id.main_frame,SettingFragment())
                transaction?.commit()
        }
+
+        change_id_btn.setOnClickListener {
+            val builder = AlertDialog.Builder(this.context!!)
+            val dialogView = layoutInflater.inflate(R.layout.activity_dialog,null)
+            val newId = dialogView.findViewById<EditText>(R.id.new_id)
+
+            builder.setView(dialogView)
+                .setPositiveButton("ID 변경"){dialogInterface, i ->
+                    checkId.text = newId.text.toString()
+                }
+                .setNegativeButton("취소"){dialogInterface, i ->
+
+                }
+                .show()
+        }
     }
 }
